b883d5f8c41f0143fe287d6d4d06f2d9bf6ee813
Mark Moskalevskij <dohaa@ukr.net>
2023-02-28 22:48:07 +0200

    retake homework

--- l10.js
+++ l10.js
@@ -1,9 +1,8 @@
-let test = prompt('создайте массив');
+let arr = prompt('укажите значения через запятую');
 
-let arr = test.split('');
-console.log(arr);
-arr.sort();
-alert(arr);
-arr.splice(2, 2);
-console.log(arr);
-alert(arr);
+let regExp = /\s*,\s*/;
+let newArr = arr.split(regExp);
+console.log('массив который задал пользователь', newArr);
+
+newArr.splice(2, 3)
+console.log('массив в котором удалили с 2 по 4 элемент', newArr);
